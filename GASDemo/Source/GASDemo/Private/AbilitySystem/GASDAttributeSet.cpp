a25f058e7780e6226c9c27bbfa99dc6c509be915
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2024-04-16 09:01:38 -0700

    Adding Health and Mana attributes

--- GASDemo/Source/GASDemo/Private/AbilitySystem/GASDAttributeSet.cpp
+++ GASDemo/Source/GASDemo/Private/AbilitySystem/GASDAttributeSet.cpp
@@ -2,4 +2,40 @@
 
 
 #include "AbilitySystem/GASDAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UGASDAttributeSet::UGASDAttributeSet()
+{
+
+}
+
+void UGASDAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASDAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASDAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASDAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGASDAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UGASDAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASDAttributeSet, Health, OldHealth);
+}
+
+void UGASDAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASDAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UGASDAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASDAttributeSet, Mana, OldMana);
+}
+
+void UGASDAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGASDAttributeSet, MaxMana, OldMaxMana);
+}
